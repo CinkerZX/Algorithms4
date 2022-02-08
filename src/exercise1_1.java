7b1ef1a181d637de524cf0f68452758efd2faaa5
CinkerZX <sindy_zhou126@126.com>
2022-02-08 13:18:44 +0100

    initial commit

---
+++ src/exercise1_1.java
@@ -0,0 +1,10 @@
+public class exercise1_1 {
+    public exercise1_1() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println(7);
+        System.out.println(200.0000002D);
+        System.out.println(true);
+    }
+}
