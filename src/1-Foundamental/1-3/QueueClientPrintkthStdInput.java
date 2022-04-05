56a08ca6dc4afd8d98c6b1f48286837fb5af4cd5
CinkerZX <sindy_zhou126@126.com>
2022-04-05 12:39:38 +0200

    Finish 1.3.15: use Queue print out the kth (from the last) of inout standard string

---
+++ src/1-Foundamental/1-3/QueueClientPrintkthStdInput.java
@@ -0,0 +1,47 @@
+import edu.princeton.cs.algs4.StdOut;
+import sun.misc.Queue;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class QueueClientPrintkthStdInput extends Queue<String> {
+
+    Queue<String> myQueue;
+    int point;
+    int length;
+
+    public QueueClientPrintkthStdInput(int k){
+        myQueue = new Queue<String>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please input at least "+ k + " strings as following format: String1 (enter) String2 (enter) ...");
+        System.out.println("Finish by entering #");
+        int i = 0;
+        while(input.hasNext()){
+            String line = input.nextLine();
+            if (line.equals("#")){
+                break;
+            }
+            myQueue.enqueue(line);
+            i++;
+        }
+        point = k;
+        length = i;
+    }
+
+    public String kthStringFromLast() throws InterruptedException {
+        int i = 0;
+        while(i<length-point){
+            myQueue.dequeue();
+            i++;
+        }
+        return myQueue.dequeue();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        QueueClientPrintkthStdInput myQ = new QueueClientPrintkthStdInput(Integer.valueOf(args[0]));
+        System.out.println(myQ.kthStringFromLast());
+    }
+}
