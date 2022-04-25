47a7f2739e3dff157601725ccf380a816bd3a278
CinkerZX <sindy_zhou126@126.com>
2022-04-25 13:21:33 +0200

    Create iterator for RandomQueue class. To do so, need to build a new constructor in RandomQueue class to copy the current RandomQueue object

--- src/1-Foundamental/1-3/RandomQueue.java
+++ src/1-Foundamental/1-3/RandomQueue.java
@@ -1,12 +1,20 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
-class RandomQueue<Item> extends ResizeArrayQueueOfStrings<Item>{
+class RandomQueue<Item> extends ResizeArrayQueueOfStrings<Item> implements Iterable<Item>{
     /**
      * Create an empty random queue
      */
     public RandomQueue() {
         super();
+    }
+
+    public RandomQueue(ArrayList<Item> arr){
+        super();
+        for (int i = 0; i < arr.size(); i++) {
+            super.add(arr.get(i));
+        }
     }
 
     /**
@@ -30,12 +38,6 @@ class RandomQueue<Item> extends ResizeArrayQueueOfStrings<Item>{
      * remove and return a random item (sample without replacement
      */
     public Item dequeue(){
-//        Random random = new Random();
-//        int N = this.getLength();
-//        int randomPointer = random.nextInt(N); //0~N-1
-//        Item picked = this.getItem(randomPointer);
-//        this.setItem(randomPointer,this.peek()); // replace
-//        this.setItem(0,picked);
         Item result = this.sample();
         this.remove_inverse();
         return result;
@@ -55,6 +57,27 @@ class RandomQueue<Item> extends ResizeArrayQueueOfStrings<Item>{
         return picked;
     }
 
+    @Override
+    public Iterator<Item> iterator(){ // Define my own iterator, necessary if want to use for each to print the result
+        return new Iterator<Item>() {
+            private ArrayList<Item> myArr = RandomQueue.super.getMyQueue();
+            private RandomQueue<Item> rq = new RandomQueue(myArr);
+            // need to build a new constructor for RandomQueue to copy the current RandomQueue object
+            // cannot refer to the current RandomQueue object
+
+            @Override // Override the original result
+            public boolean hasNext(){
+                return (!rq.isEmpty());
+            }
+
+            @Override
+            public Item next(){
+                rq.sample();
+                return (Item) rq.dequeue();
+            }
+        };
+    }
+
     public static void main(String[] args) {
         RandomQueue rq = new RandomQueue();
         System.out.println(rq.isEmpty());
@@ -62,40 +85,45 @@ class RandomQueue<Item> extends ResizeArrayQueueOfStrings<Item>{
         rq.enqueue(2);
         rq.enqueue(3);
         rq.enqueue(4);
+        // test the iterator
+        for (Object i : rq) {
+            System.out.println(i);
+        }
         System.out.println(rq.isEmpty());
-        System.out.println("Original queue");
-        for (Object i:rq) {
-            System.out.println(i);
-        }
-        System.out.println("************");
-        System.out.println("sample");
-        System.out.println(rq.sample());
-        System.out.println("************");
-        for (Object i:rq) {
-            System.out.println(i);
-        }
-        System.out.println("************");
-        System.out.println("dequeue");
-        System.out.println(rq.dequeue());
-        System.out.println("************");
-        for (Object i:rq) {
-            System.out.println(i);
-        }
-        System.out.println("************");
-        System.out.println("sample");
-        System.out.println(rq.sample());
-        System.out.println("************");
-        for (Object i:rq) {
-            System.out.println(i);
-        }
-        System.out.println("************");
-        System.out.println("dequeue");
-        System.out.println(rq.dequeue());
-        System.out.println("************");
-        for (Object i:rq) {
-            System.out.println(i);
-        }
-        System.out.println("************");
+//        System.out.println(rq.isEmpty());
+//        System.out.println("Original queue");
+//        for (Object i:rq) {
+//            System.out.println(i);
+//        }
+//        System.out.println("************");
+//        System.out.println("sample");
+//        System.out.println(rq.sample());
+//        System.out.println("************");
+//        for (Object i:rq) {
+//            System.out.println(i);
+//        }
+//        System.out.println("************");
+//        System.out.println("dequeue");
+//        System.out.println(rq.dequeue());
+//        System.out.println("************");
+//        for (Object i:rq) {
+//            System.out.println(i);
+//        }
+//        System.out.println("************");
+//        System.out.println("sample");
+//        System.out.println(rq.sample());
+//        System.out.println("************");
+//        for (Object i:rq) {
+//            System.out.println(i);
+//        }
+//        System.out.println("************");
+//        System.out.println("dequeue");
+//        System.out.println(rq.dequeue());
+//        System.out.println("************");
+//        for (Object i:rq) {
+//            System.out.println(i);
+//        }
+//        System.out.println("************");
     }
 
 
