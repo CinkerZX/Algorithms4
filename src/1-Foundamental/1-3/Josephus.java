696744e70137ff78700564f902448b62bd8a3f46
CinkerZX <sindy_zhou126@126.com>
2022-04-26 12:15:47 +0200

    Finish 1.3.37, Josephus, eliminate the Mth element of queue repeatedly

---
+++ src/1-Foundamental/1-3/Josephus.java
@@ -0,0 +1,68 @@
+import sun.misc.Queue;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class Josephus<Item> extends Queue<Item> {
+    Queue oriQueue;
+    private int N=0;
+    private int M=0;
+
+    public void setN(int n) {
+        N = n;
+    }
+
+    public void setM(int m) {
+        M = m;
+    }
+
+    public Josephus(){
+        oriQueue = new Queue();
+    }
+
+    /**
+     *
+     * @param
+     */
+    public void elimiOrder() throws InterruptedException {
+        Josephus myQueue = elimiOrderHelper_ConstructOriginQueue();
+        Queue<Item> result = new Queue<>();
+        Item temp = null;
+        int i = 0;
+        while(!myQueue.isEmpty()){
+            temp = (Item)myQueue.dequeue();
+            if (i<M-1){
+                myQueue.enqueue(temp);
+                i++;
+            }
+            else{
+                result.enqueue(temp);
+                i = 0;
+            }
+        }
+        while(!result.isEmpty()){
+            System.out.print(result.dequeue()+" ");
+        }
+    }
+
+    /**
+     * Construct the original queue
+     * @return
+     */
+    public Josephus<Item> elimiOrderHelper_ConstructOriginQueue() {
+        Josephus jQueue = new Josephus();
+        for (int i = 0; i < N; i++) {
+            jQueue.enqueue(i);
+        }
+        return jQueue;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Josephus j = new Josephus();
+        j.setN(Integer.valueOf(args[0]));
+        j.setM(Integer.valueOf(args[1]));
+        j.elimiOrder();
+    }
+}
