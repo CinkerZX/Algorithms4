21124e065a062b1ed74c41394415803f3df304e7
CinkerZX <sindy_zhou126@126.com>
2022-05-16 21:31:29 +0200

    Finish 1.4.1

---
+++ src/main/java/1-4/DoublingTest.java
@@ -0,0 +1,61 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
+/**
+ *  The {@code DoublingTest} class provides a client for measuring
+ *  the running time of a method using a doubling test.
+ *  <p>
+ *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/14analysis">Section 1.4</a>
+ *  of <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+public class DoublingTest {
+    private static final int MAXIMUM_INTEGER = 1000000;
+
+    // This class should not be instantiated.
+    private DoublingTest() { }
+
+    private static boolean containsDuplicates(int[] a, int n) { //n is the index of the last filled element of a
+        for (int i = 0; i < n; i++)
+            if (a[n] == a[i]) return true;
+        return false;
+    }
+
+    /**
+     * Returns the amount of time to call {@code ThreeSum.count()} with <em>n</em>
+     * random 6-digit integers.
+     * @param n the number of integers
+     * @return amount of time (in seconds) to call {@code ThreeSum.count()}
+     *   with <em>n</em> random 6-digit integers
+     */
+    public static double timeTrial(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = StdRandom.uniform(-MAXIMUM_INTEGER, MAXIMUM_INTEGER);
+            while (containsDuplicates(a,i)){
+                a[i] = StdRandom.uniform(-MAXIMUM_INTEGER, MAXIMUM_INTEGER);
+            }
+        }
+        Stopwatch timer = new Stopwatch();
+        ThreeSum.count(a);
+        return timer.elapsedTime();
+    }
+
+    /**
+     * Prints table of running times to call {@code ThreeSum.count()}
+     * for arrays of size 250, 500, 1000, 2000, and so forth.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        long limitTime = 60;
+        for (int n = 250; true; n += n) {
+            double time = timeTrial(n);
+            if (time>limitTime){break;}
+            StdOut.printf("%7d %7.1f\n", n, time);
+        }
+    }
+}
---
+++ src/main/java/1-4/ThreeSum.java
@@ -0,0 +1,81 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class ThreeSum {
+    private ThreeSum(){} // Do not instantiate
+
+    // returns true if the sorted array a[] contains any duplicated integers
+    private static boolean containsDuplicates(int[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (a[i] == a[i-1]) return true;
+        return false;
+    }
+
+    /**
+     * Prints to standard output the (i, j, k) with {@code i < j < k}
+     * such that {@code a[i] + a[j] + a[k] == 0}.
+     *
+     * @param a the array of integers
+     * @throws IllegalArgumentException if the array contains duplicate integers
+     */
+    public static void printAll(int[] a) {
+        int n = a.length;
+        Arrays.sort(a);
+        if (containsDuplicates(a)) throw new IllegalArgumentException("array contains duplicate integers");
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int k = Arrays.binarySearch(a, -(a[i] + a[j]));
+                if (k > j) StdOut.println(a[i] + " " + a[j] + " " + a[k]);
+            }
+        }
+    }
+
+    /**
+     * Returns the number of triples (i, j, k) with {@code i < j < k}
+     * such that {@code a[i] + a[j] + a[k] == 0}.
+     *
+     * @param a the array of integers
+     * @return the number of triples (i, j, k) with {@code i < j < k}
+     * such that {@code a[i] + a[j] + a[k] == 0}
+     */
+    public static int count(int[] a) {
+        int n = a.length;
+        int temp;
+        Arrays.sort(a);
+        if (containsDuplicates(a)) throw new IllegalArgumentException("array contains duplicate integers");
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                j = twoSum(i,j,a);
+                if (j==n){break;}
+                int k = Arrays.binarySearch(a, -(a[i] + a[j]));
+                if (k > j) count++;
+            }
+        }
+        return count;
+    }
+
+    public static int twoSum(int i, int j, int[] a){
+        if (j==a.length){return j;}
+        if ((a[i]>0 && a[j]>0 && a[i] + a[j] < a[i] && a[i] + a[j] < a[i]) || (a[i]<0 && a[j]<0 && a[i] + a[j] > a[i] && a[i] + a[j] > a[i])){
+            j++;
+            return twoSum(i, j, a);
+        }
+        return j;
+    }
+
+    /**
+     * Reads in a sequence of distinct integers from a file, specified as a command-line argument;
+     * counts the number of triples sum to exactly zero; prints out the time to perform
+     * the computation.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args)  {
+        int[] a = new int[]{1, 2, -3, 5, (int) Math.pow(2, 31),(int) Math.pow(2, 30), (int) Math.pow(-2, 31)};
+        int count = count(a);
+        StdOut.println(count);
+    }
+}
