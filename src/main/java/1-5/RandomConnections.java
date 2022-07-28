1e1fe188b42a9e7b7966992b9c6ebe0d9934de95
CinkerZX <sindy_zhou126@126.com>
2022-07-28 23:48:45 +0200

    Finish 1.5.17

--- src/main/java/1-5/RandomConnections.java
+++ src/main/java/1-5/RandomConnections.java
@@ -2,6 +2,7 @@ import com.sun.jmx.snmp.EnumRowStatus;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * The {@CODE RandomConnections} has one inner class EdroRenyi
@@ -15,12 +16,14 @@ public class RandomConnections{
     public RandomConnections(){};
 
     public static class ErdosRenyi{
-        int numConnections;
+        int numConnectionsQuickUnion;
+        int numConnectionsQuickFind;
         int numNodes;
 
         public ErdosRenyi(int N){
             numNodes = N;
-            numConnections =0;
+            numConnectionsQuickUnion = 0;
+            numConnectionsQuickFind = 0;
         }
 
         public void completeNetGenerating(){
@@ -28,14 +31,24 @@ public class RandomConnections{
             int q;
             QuickUnionUF myNet = new QuickUnionUF(numNodes);
             while(myNet.count != 1){ // not complete
-                p = (int) (Math.random() * (numNodes-1));
-                q = (int) (Math.random() * (numNodes-1));
+                p = ThreadLocalRandom.current().nextInt(0, numNodes);
+                q = ThreadLocalRandom.current().nextInt(0, numNodes);
                 if (!myNet.connected(p,q)){ // if connected
                     myNet.union(p,q);
-                    numConnections++;
+                    numConnectionsQuickUnion++;
                 }
             }
-            System.out.println(numConnections);
+            QuickFindUF myNet2 = new QuickFindUF(numNodes);
+            while(myNet2.count() != 1){ // not complete
+                p = ThreadLocalRandom.current().nextInt(0, numNodes);
+                q = ThreadLocalRandom.current().nextInt(0, numNodes);
+                if (!myNet2.connected(p,q)){ // if connected
+                    myNet2.union(p,q);
+                    numConnectionsQuickFind++;
+                }
+            }
+            System.out.println("The number of connecting operations for quickUnion is: " + numConnectionsQuickUnion);
+            System.out.println("The number of connecting operations for quickFind is: " + numConnectionsQuickFind);
         }
     }
 
@@ -43,6 +56,7 @@ public class RandomConnections{
         System.out.println("Please input the number of nodes for the net: ");
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        sc.close();
         ErdosRenyi myNet = new ErdosRenyi(N);
         myNet.completeNetGenerating();
     }
---
+++ src/main/java/1-5/RandomGridGenerator.java
@@ -0,0 +1,15 @@
+/**
+ * The {@CODE RandomGridGenerator} aims at generating random net
+ *
+ * -------------------------------------------------------------
+ * Methods
+ * RandomGrid: generate an N-by-N grid
+ * The elements for the grid are the pair of nodes, (p,q), which is of Connection class
+ *
+ * Generate(int N): return the N-by-N matrix
+ *
+ * main(): take N from command line, call Generate(N), and print out the connections by iterating through the returned matrix
+ */
+public class RandomGridGenerator {
+
+}
