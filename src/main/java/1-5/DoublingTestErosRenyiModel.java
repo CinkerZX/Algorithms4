706be97a592265b68d64a4eced185a981f5cb60a
CinkerZX <sindy_zhou126@126.com>
2022-08-09 23:43:07 +0200

    1.5.23-1.5.26 included in 1.5.22. Finished 1.5!

--- src/main/java/1-5/DoublingTestErosRenyiModel.java
+++ src/main/java/1-5/DoublingTestErosRenyiModel.java
@@ -1,6 +1,10 @@
+import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.OptionalDouble;
 import java.util.Scanner;
 
 /**
@@ -76,6 +80,72 @@ public class DoublingTestErosRenyiModel {
         System.out.println(df.format(totalTime/(T*aveCountWeightedQuickUnion)));
     }
 
+    public static double[] timeConsume(int T, int N){
+        //TODO: run the three experiments and return the results
+        double[] runningTime = new double[3]; // quickFind, quickUnion, weightedQuickUnion
+
+        // QuickFind
+        long startTime = System.nanoTime();
+        testQuickFind(N,T);
+        long endTime   = System.nanoTime();
+        long totalTime = endTime - startTime;
+        runningTime[0] = totalTime/(1000*T);
+
+        // QuickUnion
+        startTime = System.nanoTime();
+        testQuickUnion(N,T);
+        endTime   = System.nanoTime();
+        totalTime = endTime - startTime;
+        runningTime[1] = totalTime/(1000*T);
+
+        //weightedQuickUnion
+        startTime = System.nanoTime();
+        testWeightedQuickUnion(N,T);
+        endTime   = System.nanoTime();
+        totalTime = endTime - startTime;
+        runningTime[2] = totalTime/(1000*T);
+
+        return runningTime;
+    }
+
+    public static double[][] trailExp(int T){
+        //TODO: test N = 10, 20, 30, 40.. 1500
+        double[][] runingTime = new double[3][10]; // 3 rows: quickFind, quickUnion, weightedQuickUnion
+        double[] oneExp = new double[3];
+        for (int i = 1; i <= 10; i++) {
+            oneExp = timeConsume(T, 10*i); // run one trail
+            for (int j = 0; j < 3; j++) {
+                runingTime[j][i-1] = oneExp[j]; // save the results
+            }
+        }
+        return runingTime;
+    }
+
+    public static void visualize(double[][] timeConsume, int N) { // N = 1500
+        //TODO: draw eachFinal & average total Final
+        double[] timeConsumeQF = timeConsume[0];
+        double[] timeConsumeQU = timeConsume[1];
+        double[] timeConsumeWQU = timeConsume[2];
+        double[] x = new double[N];
+        double Yscale = Arrays.stream(timeConsumeQF).max().getAsDouble();
+        StdDraw.setCanvasSize(1000,1000);
+        StdDraw.setXscale(-5,N+5);
+        StdDraw.setYscale(-5,Yscale+5);
+        StdDraw.line(-5,0,N+5,0); // x-axis
+        StdDraw.line(0,-5,0,Yscale+5); // y-axis
+        StdDraw.setPenRadius(0.01);
+
+        for (int i = 1; i <= N/10; i++) {
+            x[i] = i*10;
+            StdDraw.setPenColor(StdDraw.GRAY);
+            StdDraw.point(x[i], timeConsumeQF[i-1]);
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.point(x[i], timeConsumeQU[i-1]);
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.point(x[i], timeConsumeWQU[i-1]);
+        }
+    }
+
     public static void main(String[] args) {
         int T = 0;
         try {
@@ -85,47 +155,17 @@ public class DoublingTestErosRenyiModel {
             System.exit(1);
         }
         int N = 10;
-//        printEverything(T,N);
-//        System.out.println("********************************");
-//        N = 20;
-//        printEverything(T,N);
-//        System.out.println("********************************");
-//        N = 40;
-//        printEverything(T,N);
-//        System.out.println("********************************");
-//        N = 80;
-//        printEverything(T,N);
-//        System.out.println("********************************");
-//        N = 160;
-//        printEverything(T,N);
-//        System.out.println("********************************");
-//        N = 320;
+
+//        N = 500;
 //        printEverything(T,N);
 //        System.out.println("********************************");
 //
-//        N = 640;
+//        N = 800;
 //        printEverything(T,N);
 //        System.out.println("********************************");
 
-        N = 500;
-        printEverything(T,N);
-        System.out.println("********************************");
-
-        N = 800;
-        printEverything(T,N);
-        System.out.println("********************************");
-
-        N = 1000;
-        printEverything(T,N);
-        System.out.println("********************************");
-
-        N = 1200;
-        printEverything(T,N);
-        System.out.println("********************************");
-
-        N = 1400;
-        printEverything(T,N);
-        System.out.println("********************************");
+        double[][] timeConsume = trailExp(T);
+        visualize(timeConsume,100);
 
     }
 
