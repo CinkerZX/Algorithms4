1ccac67b552f18d270b6b00aabcbc78717019b1a
CinkerZX <sindy_zhou126@126.com>
2022-08-23 23:06:27 +0200

    Finish 2.1.12

--- src/main/java/2-1/ShellSort.java
+++ src/main/java/2-1/ShellSort.java
@@ -1,11 +1,14 @@
 import com.sun.org.apache.xalan.internal.xsltc.compiler.util.CompareGenerator;
 
+import java.math.BigInteger;
+
 /**
  * The {@CODE ShellSort} is an implementation of ShellSort algorithm
  *
  * function sort is a static method, which doesn't need any instantiation
  */
 public class ShellSort{
+
     public static void sort(Comparable[] a){
         //TODO: Sort a[] in an ascending order
         int N = a.length;
@@ -15,23 +18,43 @@ public class ShellSort{
             // h-sort the array
             for (int i = h; i < N; i++) {
                 //TODO: insert a[i] among a[i-h], a[i-2*h], a[i-3*h]...
-                for (int j = i; j >= h && less(a[j],a[j-h]); j -= h) { // i = 13
-                    each(a, j, j-h);
+                for (int j = i; j >= h && less(a[j],a[j-h]); j -= h) {
+                    exch(a, j, j-h);
                 }
-                h = h/3;
             }
+            h = h/3;
         }
+    }
+
+    public static BigInteger sortNumCompare(Comparable[] a){
+        BigInteger numComprea = new BigInteger("0");
+        //TODO: Sort a[] in an ascending order
+        int N = a.length;
+        int h = 1;
+        while(h<N/3) h = 3*h+1; // h = 1, 4, 13, 40, 121, ....  depends on the length of a
+        while(h >= 1){//TODO: Start from the largest h
+            // h-sort the array
+            for (int i = h; i < N; i++) {
+                //TODO: insert a[i] among a[i-h], a[i-2*h], a[i-3*h]...
+                for (int j = i; j >= h; j -= h) {
+                    if(less(a[j],a[j-h])){
+                        exch(a, j, j-h);
+                    }
+                    numComprea = numComprea.add(BigInteger.ONE); // count the num of compare
+                }
+            }
+            h = h/3;
+        }
+        return numComprea;
     }
 
     public static boolean less(Comparable a, Comparable b){
-        if(a.compareTo(b) < 0){
-            return true;
-        }
-        else
-            return false;
+        return a.compareTo(b) <0;
     }
 
-    public static void each(Comparable[] a, int i, int j){
-
+    public static void exch(Comparable[] a, int i, int j){
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 }
--- src/main/java/2-1/visShellsortNumComparesbyArraySize.java
+++ src/main/java/2-1/visShellsortNumComparesbyArraySize.java
@@ -1,3 +1,10 @@
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * The {@CODE visShellsortNumComparebyArraySize} class aims at testing the hypothesis that #Compares / size(array)
  * is a small constant
@@ -11,5 +18,65 @@
  *
  */
 public class visShellsortNumComparesbyArraySize {
+
+    public static BigDecimal rateNumComparewithN(Comparable[] a){
+        BigDecimal result = new BigDecimal(ShellSort.sortNumCompare(a));
+        BigDecimal n = new BigDecimal(a.length);
+        return result.divide(n);
+    }
+
+    public static Comparable[] randomArrayGenerator(int n){
+        Random random = new Random();
+        Comparable[] result = new Double[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = random.nextDouble();
+        }
+        return result;
+    }
+
+    public static void trails(){
+        int n = 1;
+        int powerTimes = 8;
+        StdDraw.setCanvasSize(1000,1000);
+        StdDraw.setXscale(-1,powerTimes);
+        StdDraw.setYscale(-0.5,2);
+        StdDraw.line(-1,0,powerTimes,0); // x-axis
+        StdDraw.line(0,-0.5,0,2); // y-axis
+        StdDraw.setPenRadius(0.01);
+
+        for (int i = 0; i < powerTimes; i++) { // 10 100 1000 10000 ... 100000000
+            n = n*10;
+            double x = i;
+            BigDecimal scale = new BigDecimal(n);
+            double y = rateNumComparewithN(randomArrayGenerator(n)).divide(scale).doubleValue();
+            System.out.println(y);
+            StdDraw.setPenColor(StdDraw.GRAY);
+            StdDraw.point(x, y);
+        }
+    }
+
+    public static void main(String[] args) {
+        Comparable[] a = randomArrayGenerator(10);
+        BigDecimal n = new BigDecimal(a.length);
+        System.out.println(rateNumComparewithN(a).divide(n));
+
+        a = randomArrayGenerator(100);
+        n = new BigDecimal(a.length);
+        System.out.println(rateNumComparewithN(a).divide(n));
+
+        a = randomArrayGenerator(1000);
+        n = new BigDecimal(a.length);
+        System.out.println(rateNumComparewithN(a).divide(n));
+
+        a = randomArrayGenerator(10000);
+        n = new BigDecimal(a.length);
+        System.out.println(rateNumComparewithN(a).divide(n));
+
+        a = randomArrayGenerator(100000);
+        n = new BigDecimal(a.length);
+        System.out.println(rateNumComparewithN(a).divide(n));
+
+//        trails();
+    }
 
 }
