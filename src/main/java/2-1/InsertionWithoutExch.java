82da4ec6d6a9bada64a004792cdc36b7cf3770e6
CinkerZX <sindy_zhou126@126.com>
2022-09-17 23:38:11 +0200

    2.1.25 Finish sort function

---
+++ src/main/java/2-1/InsertionWithoutExch.java
@@ -0,0 +1,173 @@
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * The {@Code InsertionWithoutExch} implement insertion alg without using exch()
+ *
+ * Idea: add one position on the right side of the original array, sort from right to left:
+ *       eg: 2 3 1 5 4 _
+ *       put the larger one on the right empty place
+ *       if the larger one is the one with smaller index, then move the smaller item one position left
+ *       eg: 2 3 1 4 _ 5
+ *           2 3 1 _ 4 5
+ *           2 1 _ 3 4 5
+ *           1 _ 2 3 4 5 until reaching index 0, move to position 1, and return array 1:n
+ */
+public class InsertionWithoutExch {
+    // This class should not be instantiated.
+    public InsertionWithoutExch() { }
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     * @param a the array to be sorted
+     */
+    public static void sort(Comparable[] a) {
+        int n = a.length;
+        Comparable[] temp = new Comparable[n+1];
+        for (int i = 0; i < n; i++) {
+            temp[i] = a[i];
+        }
+        for (int i = n-1; i > 0; i--) {
+            if (less(a[i],a[i-1])){// if the a[i-1] > a[i]
+                a[i+1] = a[i-1];
+                a[i-1] = a[i];
+            }
+            else{
+                a[i+1] = a[i];
+            }
+        }
+        a = Arrays.copyOfRange(temp, 1, n+1);
+        assert isSorted(a);
+    }
+/************************************************* Have not changed yet ******************************
+    /**
+     * Rearranges the subarray a[lo..hi) in ascending order, using the natural order.
+     * @param a the array to be sorted
+     * @param lo left endpoint (inclusive)
+     * @param hi right endpoint (exclusive)
+     */
+    public static void sort(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i < hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j-1]); j--) {
+                exch(a, j, j-1);
+            }
+        }
+        assert isSorted(a, lo, hi);
+    }
+
+    /**
+     * Rearranges the array in ascending order, using a comparator.
+     * @param a the array
+     * @param comparator the comparator specifying the order
+     */
+    public static void sort(Object[] a, Comparator comparator) {
+        int n = a.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j-1], comparator); j--) {
+                exch(a, j, j-1);
+            }
+            assert isSorted(a, 0, i, comparator);
+        }
+        assert isSorted(a, comparator);
+    }
+
+    /**
+     * Rearranges the subarray a[lo..hi) in ascending order, using a comparator.
+     * @param a the array
+     * @param lo left endpoint (inclusive)
+     * @param hi right endpoint (exclusive)
+     * @param comparator the comparator specifying the order
+     */
+    public static void sort(Object[] a, int lo, int hi, Comparator comparator) {
+        for (int i = lo + 1; i < hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j-1], comparator); j--) {
+                exch(a, j, j-1);
+            }
+        }
+        assert isSorted(a, lo, hi, comparator);
+    }
+
+
+    // return a permutation that gives the elements in a[] in ascending order
+    // do not change the original array a[]
+    /**
+     * Returns a permutation that gives the elements in the array in ascending order.
+     * @param a the array
+     * @return a permutation {@code p[]} such that {@code a[p[0]]}, {@code a[p[1]]},
+     *    ..., {@code a[p[n-1]]} are in ascending order
+     */
+    public static int[] indexSort(Comparable[] a) {
+        int n = a.length;
+        int[] index = new int[n];
+        for (int i = 0; i < n; i++)
+            index[i] = i;
+
+        for (int i = 1; i < n; i++)
+            for (int j = i; j > 0 && less(a[index[j]], a[index[j-1]]); j--)
+                exch(index, j, j-1);
+
+        return index;
+    }
+
+    /***************************************************************************
+     *  Helper sorting functions.
+     ***************************************************************************/
+
+    // is v < w ?
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    // is v < w ?
+    private static boolean less(Object v, Object w, Comparator comparator) {
+        return comparator.compare(v, w) < 0;
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    // exchange a[i] and a[j]  (for indirect sort)
+    private static void exch(int[] a, int i, int j) {
+        int swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    /***************************************************************************
+     *  Check if array is sorted - useful for debugging.
+     ***************************************************************************/
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length);
+    }
+
+    // is the array a[lo..hi) sorted
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i < hi; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+
+    private static boolean isSorted(Object[] a, Comparator comparator) {
+        return isSorted(a, 0, a.length, comparator);
+    }
+
+    // is the array a[lo..hi) sorted
+    private static boolean isSorted(Object[] a, int lo, int hi, Comparator comparator) {
+        for (int i = lo + 1; i < hi; i++)
+            if (less(a[i], a[i-1], comparator)) return false;
+        return true;
+    }
+
+    // print array to standard output
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+}
