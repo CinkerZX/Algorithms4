d8cf8f6d112929a6978fe3aee8884be015eb41cc
CinkerZX <sindy_zhou126@126.com>
2023-09-11 06:48:57 +0200

    Finish Improvement of merge -- without cope

--- src/main/java/2-2/ImproveMergeSort.java
+++ src/main/java/2-2/ImproveMergeSort.java
@@ -61,83 +61,81 @@ public class ImproveMergeSort {
         }
     }
 
-
     // Idea: Eliminate the copy to the auxiliary
     public static void ImpSortBottomUpNoCopy(Comparable[] a){
         //TODO: use two for loop to merge
         int N = a.length;
         for (int i = 1; i < N; i = i+i) { // i-- subarray size
             for (int lo = 0; lo < N-i ; lo += i+i) {
-                mergeNoAux(a, mergeNoAux(a, lo, lo+i-1, Math.min(lo+i-1+i, N-1))); // hi -- the mid+i, if it out of index, hi = N-1
+                mergeNoAux(a, lo, mergeNoAux(a, lo, lo+i-1, Math.min(lo+i-1+i, N-1))); // hi -- the mid+i, if it out of index, hi = N-1
             }
         }
     }
 
-    // given array => sorted array
-    public static void mergeNoAux(Comparable[] a, Comparable[] sortedAux){
+    // sorted array assign into the given array
+    public static void mergeNoAux(Comparable[] a, int start, Comparable[] sortedAux){
         //Todo: copy sortedAux into a[]
-        for (int i = 0; i < a.length; i++) {
-            a[i] = sortedAux[i];
+        for (int i = start; i < sortedAux.length ; i++) {
+            if (sortedAux[i] != null) a[i] = sortedAux[i];
+            else break;
         }
     }
 
-    // sort a, put the result into myaux
+    // sort given array, put the result into myaux
     public static Comparable[] mergeNoAux(Comparable[] a, int lo, int mid, int hi){
         //Todo: merge array a[lo, mid] and a[mid+1, hi]
         Comparable[] sortedAux = new Comparable[a.length];
         int i = lo, j = mid+1;
-        for (int k = 0; k <= hi ; k++) {
-            if (i==mid){ // a[li, mid] is cleaned
-                sortedAux[k] = a[j++];
-            }
-            else {
-                if (j==hi){ // a[mid+1, hi] is cleaned
-                    sortedAux[k] = a[i++];
-                }
-                else{
-                    if(sort.less(a[j],a[i])){
-                        sortedAux[k] = a[j++];
-                    }
-                    else{
-                        sortedAux[k] = a[i++];
-                    }
-                }
-            }
+        for (int k = lo; k <= hi; k++) {
+            if (i>mid)  sortedAux[k] = a[j++];
+            else if (j>hi) sortedAux[k] = a[i++];
+            else if(sort.less(a[j],a[i])) sortedAux[k] = a[j++];
+            else sortedAux[k] = a[i++];
         }
         return(sortedAux);
     }
 
+    //****************** Amend3 Combine Merge with Insertion *******************
+
     public static void main(String[] args) {
         // Test of Amend1
-        String[] a = new String[]{"E", "E", "G", "M", "R", "A", "C", "E", "R", "T"};
-        sort.mergeInsert(a, 0, 4, 9);
-        System.out.println("Merge combine with Insertion");
-        sort.printStringArray(a);
+//        String[] a = new String[]{"E", "E", "G", "M", "R", "A", "C", "E", "R", "T"};
+//        sort.mergeInsert(a, 0, 4, 9);
+//        System.out.println("Merge combine with Insertion");
+//        sort.printStringArray(a);
+//
+//        // Test of sortTopdown
+//        a = new String[]{"M", "E", "R", "G", "E", "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+//        ImpSortTopDownInsert(a);
+//        sort.printStringArray(a);
+//
+//        // Test of sortBottomUp
+//        a = sort.generateStringArray(10);
+//        ImpSortBottomUpInsert(a);
+//        sort.printStringArray(a);
+//
+//        //Test of Amend2
+//        a = new String[]{"E", "E", "G", "M", "R", "A", "C", "E", "R", "T"};
+//        sort.mergeCheckOrder(a, 0, 4, 9);
+//        System.out.println("Check if ordered before merge");
+//        sort.printStringArray(a);
+//
+//        // Test of sortTopdown
+//        a = new String[]{"M", "E", "R", "G", "E", "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+//        ImpSortTopDownCheckOrder(a);
+//        sort.printStringArray(a);
+//
+//        // Test of sortBottomUp
+//        a = sort.generateStringArray(10);
+//        ImpSortBottomUpCheckOrder(a);
+//        sort.printStringArray(a);
 
-        // Test of sortTopdown
-        a = new String[]{"M", "E", "R", "G", "E", "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
-        ImpSortTopDownInsert(a);
+        // Test of ImpSortBottomUpNoCopy
+        String[] a = new String[]{"F", "E", "A"};
+        System.out.println("Check ImpSortBottomUpNoCopy");
+//        a = sort.generateStringArray(5);
         sort.printStringArray(a);
-
-        // Test of sortBottomUp
-        a = sort.generateStringArray(10);
-        ImpSortBottomUpInsert(a);
-        sort.printStringArray(a);
-
-        //Test of Amend2
-        a = new String[]{"E", "E", "G", "M", "R", "A", "C", "E", "R", "T"};
-        sort.mergeCheckOrder(a, 0, 4, 9);
-        System.out.println("Check if ordered before merge");
-        sort.printStringArray(a);
-
-        // Test of sortTopdown
-        a = new String[]{"M", "E", "R", "G", "E", "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
-        ImpSortTopDownCheckOrder(a);
-        sort.printStringArray(a);
-
-        // Test of sortBottomUp
-        a = sort.generateStringArray(10);
-        ImpSortBottomUpCheckOrder(a);
+        ImpSortBottomUpNoCopy(a);
         sort.printStringArray(a);
     }
 
